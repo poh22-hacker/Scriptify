89daf6a713098f75a3c3932ff17268cecdf47a05
poh22-hacker <felipenegrireis.fnr@gmail.com>
2025-01-17 19:31:15 -0300

    Scriptify.py

--- python.py
+++ python.py
@@ -73,6 +73,8 @@ S       C      R   R    I   P   P    T    I   F       Y Y
     elif subpergunta == "6":
         print("Atualizando")
         os.system('clear')
+        os.system('cd')
+        os.system('rm -rf Scriptfy')
         os.system('https://github.com/poh22-hacker/Scriptify.git')
 
     else:
